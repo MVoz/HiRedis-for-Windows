12171deb3d40db15df8a6cbabe0af4812e7e6427
Voskrese <neustroev@gmail.com>
2019-04-04 10:32:02 +0800

    Update win32_hiredis.h

--- win32_hiredis.h
+++ win32_hiredis.h
@@ -23,10 +23,10 @@
 #ifndef WIN32_HIREDIS_H
 #define WIN32_HIREDIS_H
 
-#include "../../src/Win32_Interop/Win32_Portability.h"
-#include "../../src/Win32_Interop/Win32_types_hiredis.h"
-#include "../../src/Win32_Interop/Win32_Error.h"
-#include "../../src/Win32_Interop/Win32_FDAPI.h"
+#include "Win32_Interop/Win32_Portability.h"
+#include "Win32_Interop/Win32_types_hiredis.h"
+#include "Win32_Interop/Win32_Error.h"
+#include "Win32_Interop/Win32_FDAPI.h"
 #define INCL_WINSOCK_API_PROTOTYPES 0 // Important! Do not include Winsock API definitions to avoid conflicts with API entry points defined below.
 #include <WinSock2.h>                 // For SOCKADDR_STORAGE
 
